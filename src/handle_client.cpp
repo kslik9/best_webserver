7c595d126231ad3ddab4f10afea3f5444b360006
youssef labrahmi <ylabrahm@student.1337.ma>
2023-11-02 18:36:02 +0100

    optimization

--- src/handle_client.cpp
+++ src/handle_client.cpp
@@ -1,47 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   handle_client.cpp                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aessaoud <aessaoud@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: Invalid date        by    #+#            #+#    #+#             */
-/*   Updated: 2023/11/02 00:43:10 by aessaoud         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "webserv.hpp"
 
-std::string pars_method(std::string &buffer, int &pos)
+void parse_request(const std::string &request, std::string &method, std::string &target)
 {
-    return (buffer.substr(0, pos));
-}
-std::string pars_target(std::string &buffer, int &pos)
-{
-    int i = (pos + 1);
-    std::string target;
-    while (buffer[i] != ' ')
-        target += buffer[i++];
-    target = target.substr(1, target.length() - 1);
-    return (target);
+    try
+    {
+        std::istringstream iss(request);
+        iss >> method >> target;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        method = "GET";
+        target = "/";
+    }
 }
 
 void ft_handle_client(int client_fd)
 {
     if (client_fd >= 0)
     {
+        // -----------------------------------------------------
         char *buffer = new char[BUFFER_SIZE];
         ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
+        // -----------------------------------------------------
         if (bytes_received > 0)
         {
+            // -----------------------------------------------------
             std::string str_buffer(buffer), target, method;
             int pos = str_buffer.find(" ");
-            method = pars_method(str_buffer, pos);
-            target = pars_target(str_buffer, pos);
-            // ------------------------------------------------------------------------------------------------
+            // -----------------------------------------------------
+            parse_request(str_buffer, method, target);
+            // -----------------------------------------------------
             std::string http_resp = buildHttpResponse(method, target);
             send(client_fd, http_resp.c_str(), http_resp.length(), 0);
-            // ------------------------------------------------------------------------------------------------
+            // -----------------------------------------------------
             close(client_fd);
         }
         delete[] buffer;
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,42 @@
+
 #include "webserv.hpp"
 
-const char *sIp = "192.168.1.100";
-
-int main(int __unused argc, char const __unused *argv[])
+int main(int argc, char const *argv[])
 {
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1)
     {
-        std::cerr << "Error creating socket" << std::endl;
-        return (EXIT_FAILURE);
+        std::cerr << "Error creating socket" << '\n';
+        return EXIT_FAILURE;
     }
-    // --------------------------------------------------------------
+    // -----------------------------------------------------
     sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080);
     server_address.sin_addr.s_addr = INADDR_ANY;
-    // server_address.sin_addr.s_addr = inet_addr("0.0.0.0");   ----  inet_addr converts ip address
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
     {
-        std::cerr << "Error binding socket" << std::endl;
+        std::cerr << "Error binding socket" << '\n';
         close(server_socket);
-        return (EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
-    // --------------------------------------------------------------
+    // -----------------------------------------------------
     if (listen(server_socket, 5) == -1)
     {
-        std::cerr << "Error listening on socket" << std::endl;
+        std::cerr << "Error listening on socket" << '\n';
         close(server_socket);
-        return (EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
-    std::cout << "Server is listening on 0.0.0.0:8080" << std::endl;
-    // --------------------------------------------------------------
+    // -----------------------------------------------------
+    std::cout << "Server is listening on 0.0.0.0:8080" << '\n';
+    // -----------------------------------------------------
     while (1)
     {
-        sockaddr *client_addrr = new sockaddr();
-        socklen_t *client_addrr_len = new socklen_t();
-        *client_addrr_len = sizeof(*client_addrr);
-        int client_fd = accept(server_socket, client_addrr, client_addrr_len);
+        sockaddr client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_fd = accept(server_socket, &client_addr, &client_addr_len);
         ft_handle_client(client_fd);
-        delete client_addrr;
-        delete client_addrr_len;
     }
-    // --------------------------------------------------------------
+    // -----------------------------------------------------
     return 0;
 }
