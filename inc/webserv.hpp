8a0929e4a573f993c1a420b232b486f1673c0220
Achraf Essaoudi <aessaoud@c2r5p1.1337.ma>
2023-11-06 15:47:30 +0100

    select() was added - v1

--- inc/webserv.hpp
+++ inc/webserv.hpp
@@ -14,6 +14,9 @@
 #include <arpa/inet.h>
 #include <string>
 #include <map>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/select.h>
 
 // Consts
 #define BUFFER_SIZE (1024 * 1024) // 1MB
