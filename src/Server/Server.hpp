0b1809c5deb14fffeafcc04ee845589e3c6c3b72
youssef labrahmi <ylabrahm@c2r6p8.1337.ma>
2023-11-10 21:53:29 +0100

    Finally, some OOP

--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -1,20 +1,28 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-#include <iostream>
-#include <string>
+#include "webserv.hpp"
+
+class Config;
 
 class Server
 {
-	int SocketFd;
+private:
+	Config &config;
+	unsigned int SocketFd;
+	unsigned int activeClients;
+	sockaddr_in server_address;
+
 public:
-	Server();
+	Server(Config &config);
 	Server(Server const &src);
 	~Server();
-
+	void start();
+	void waitClients();
+	unsigned int getSocketFd() const;
+	Config getConfig() const;
+	sockaddr_in getServer_address() const;
 	Server &operator=(Server const &rhs);
-
-private:
 };
 
 #endif /* ********************************************************** SERVER_H */
