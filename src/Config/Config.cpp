e3c97a21ad3d2f335ab7cb308f4bb6b5eedd5e7c
kslik9 <khalid.slik@usmba.ac.ma>
2023-11-15 12:10:00 +0100

    parse config file , part of locations

--- src/Config/Config.cpp
+++ src/Config/Config.cpp
@@ -11,14 +11,58 @@ Config::Config()
 Config::Config(const Config &src)
 {
 }
+int didWeFinish(std::string tmp)
+{
+	for(size_t i = 0; i < tmp.length(); i++)
+	{
+		if(tmp[i] == ']')
+			return 1;
+		else if(tmp[i] != ' ')
+			return 2;
+	}
+	return 0;
+}
+std::string Config::value_fo_loca(vec::iterator it)
+{
+	*it++;
+	*it++;
+	std::string map;
+	std::string tmp;
+	tmp = *it;
+	while(didWeFinish(tmp) != 1)
+	{
+		map = map + tmp;
+		++it;
+		tmp = *it;
+	}
+	return map;
+}
 void Config::parseConf()
 {
-	// std::vector<std::string>::iterator it = this->raw_data.begin();
-	// while(it != this->raw_data.end())
-	// {
-	// 	std::cout << *it << std::endl;
-	// 	it++;
-	// }
+	vec::iterator it = this->raw_data.begin();
+	std::string tmp;
+	std::string value;
+	while(it != this->raw_data.end())
+	{
+		tmp = *it;
+		size_t loc = tmp.find("location");
+		if(loc != std::string::npos)
+		{
+			size_t pos = tmp.find('/');
+			tmp = tmp.substr(pos - 1);
+
+			value = value_fo_loca(it);
+			srvConf.routes[tmp] = value;
+		}
+		it++;
+	}
+	mp::iterator pop;
+	pop = srvConf.routes.begin();
+	while(pop != srvConf.routes.end())
+	{
+		std::cout << pop->first << " " << pop->second << std::endl;
+		pop++;
+	}
 
 }
 Config::Config(std::string conf)
@@ -37,6 +81,7 @@ Config::Config(std::string conf)
 			this->raw_data.push_back(line);
 		parseConf();
 	}
+	exit(0);
 }
 
 /*
