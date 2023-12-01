02b4e779e976bee7bc3938475c93f35ff2d1c632
Achraf Essaoudi <aessaoud@c2r5p1.1337.ma>
2023-12-01 13:25:31 +0100

    poll now works well

--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -10,17 +10,15 @@ class HttpMessageChecker;
 class Server
 {
 	private:
-		Config &config;
-		int socketFd;
-		unsigned int activeClients;
-		sockaddr_in server_address;
+		Config				&config;
+		sockaddr_in			server_address;
+		std::vector<int>	serverSocketsFd;
 	public:
 		Server(Config &config);
 		Server(Server const &src);
 		~Server();
 		void start();
 		void waitClients();
-		unsigned int getSocketFd() const;
 		Config getConfig() const;
 		sockaddr_in getServer_address() const;
 		std::string buildHttpResponse(std::string request);
