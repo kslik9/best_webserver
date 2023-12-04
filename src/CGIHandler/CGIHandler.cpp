e42d454b6d81bdf95761d15798d9ab27fe332396
youssef labrahmi <ylabrahm@c2r6p8.1337.ma>
2023-12-04 18:10:54 +0100

    env variables has to be dynamic

--- src/CGIHandler/CGIHandler.cpp
+++ src/CGIHandler/CGIHandler.cpp
@@ -49,7 +49,7 @@ std::string CGIHandler::process()
 	{
 		std::cerr << "Error opening the file: " << src.path << std::endl;
 		delete src.file_stream;
-		return "";
+		return "delete";
 	}
 	php_args.push_back(CGI_BIN);
 	php_args.push_back(src.path.c_str());
@@ -59,7 +59,7 @@ std::string CGIHandler::process()
 	{
 		std::cerr << "Error creating pipe" << std::endl;
 		delete src.file_stream;
-		return "";
+		return "delete";
 	}
 	pid_t child_pid = fork();
 	if (child_pid == -1)
@@ -68,17 +68,18 @@ std::string CGIHandler::process()
 		close(pipe_fd[0]);
 		close(pipe_fd[1]);
 		delete src.file_stream;
-		return "";
+		return "delete";
 	}
 	if (child_pid == 0)
 	{
+		std::cout << "78: " << this->env_vec.data()[5] << std::endl;
 		close(pipe_fd[0]);
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
-		if (execve(CGI_BIN, (char* const*)(&php_args[0]), (char* const*)(this->env_vec.data())) == -1)
+		if (execve(CGI_BIN, (char *const *)(&php_args[0]), (char *const *)(this->env_vec.data())) == -1)
 		{
 			std::cerr << "Error executing php command" << std::endl;
-			_exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
 		}
 	}
 	else
@@ -92,10 +93,6 @@ std::string CGIHandler::process()
 		close(pipe_fd[0]);
 		int status;
 		waitpid(child_pid, &status, 0);
-		if (WIFEXITED(status))
-			std::cout << "Child process exited with status: " << WEXITSTATUS(status) << std::endl;
-		else
-			std::cerr << "Child process did not exit normally" << std::endl;
 		delete src.file_stream;
 		return output.str();
 	}
@@ -104,28 +101,46 @@ std::string CGIHandler::process()
 
 void CGIHandler::init_env(RequestData request)
 {
-	std::cout << "req: [" << request << "]\n";
-	// exit(1);
-	this->env_vec.push_back(("REQUEST_URI=" + request.getUri()).c_str());	 // + request.getPath().substr(0, request.getPath().find_last_of("/")));
-	this->env_vec.push_back("DOCUMENT_URI=");	 // + request.getPath() + request.getQueryString());
-	this->env_vec.push_back("SCRIPT_NAME=/index.php");	 // +  request.getPath());
-	this->env_vec.push_back("SCRIPT_FILENAME=/Users/aessaoud/Desktop/webserv/php/index.php"); // + response.get_full_path());
-	this->env_vec.push_back("PATH_TRANSLATED="); // + response.get_full_path());
-	this->env_vec.push_back("QUERY_STRING=");	 // + request.getQueryString());
+	this->env_vec.push_back("DOCUMENT_URI=/blog/post");
+	this->env_vec.push_back("SCRIPT_NAME=/index.php");
+	this->env_vec.push_back("SCRIPT_FILENAME=/Users/ylabrahm/Desktop/webserv/var/www/html/blog/post/index.php");
 	this->env_vec.push_back("SERVER_NAME=0.0.0.0");
-	this->env_vec.push_back("SERVER_PORT=8081");	// + std::to_string(request.getPort()));
-	this->env_vec.push_back(("REQUEST_METHOD=" + request.getMethod()).c_str()); // + request.getMethod());
-	this->env_vec.push_back("DOCUMENT_ROOT=/Users/aessaoud/Desktop/webserv/php");	// + response.get_root());
+	this->env_vec.push_back("SERVER_PORT=8081");
+	this->env_vec.push_back("REQUEST_METHOD= + request.getMethod()).c_str()");
+	this->env_vec.push_back("DOCUMENT_ROOT=GET");
 	this->env_vec.push_back("GETAWAY_INTERFACE=CGI/1.1");
 	this->env_vec.push_back("SERVER_PROTOCOL=HTTP/1.1");
 	this->env_vec.push_back("REDIRECT_STATUS=200");
 	this->env_vec.push_back("FCGI_ROLE=RESPONDER");
 	this->env_vec.push_back("REQUEST_SCHEME=http");
 	this->env_vec.push_back("SERVER_SOFTWARE=webserv/1.1");
-	this->env_vec.push_back(("PATH=" + std::string(std::getenv("PATH"))).c_str());
+	this->env_vec.push_back("PATH=/Users/ylabrahm/goinfre/homebrew/bin:/Users/ylabrahm/goinfre/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki");
 	this->env_vec.push_back("REMOTE_ADDR=0.0.0.0");
 	this->env_vec.push_back("REMOTE_PORT=0");
+	this->env_vec.push_back("PATH_TRANSLATED=");
+	this->env_vec.push_back("QUERY_STRING=");
 	this->env_vec.push_back(NULL);
+	// ------------------ http://localhost/blog/post?id=123 ------------------ //
+	// REQUEST_URI=/blog/post?id=123
+	// DOCUMENT_URI=/blog/post
+	// SCRIPT_NAME=/index.php
+	// SCRIPT_FILENAME=/var/www/html/index.php
+	// PATH_TRANSLATED=/var/www/html/index.php
+	// QUERY_STRING=id=123
+	// SERVER_NAME=example.com
+	// SERVER_PORT=80
+	// REQUEST_METHOD=GET
+	// DOCUMENT_ROOT=/var/www/html
+	// GATEWAY_INTERFACE=CGI/1.1
+	// SERVER_PROTOCOL=HTTP/1.1
+	// REDIRECT_STATUS=200
+	// FCGI_ROLE=RESPONDER
+	// REQUEST_SCHEME=http
+	// SERVER_SOFTWARE=Apache/2.4.29
+	// PATH=/usr/local/bin:/usr/bin:/bin
+	// REMOTE_ADDR=192.168.1.100
+	// REMOTE_PORT=12345
+	// ----------------------------------------------------------------------- //
 }
 
 /*
