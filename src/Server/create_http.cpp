7ae2fdc06cd0b4e9a59d95306dbf7110ff42a852
Youssef Labrahmi <yousseflabrahmi8@gmail.com>
2023-12-07 12:32:02 +0100

    starting upload

--- src/Server/create_http.cpp
+++ src/Server/create_http.cpp
@@ -130,10 +130,11 @@ std::string Server::buildHttpResponse(int socketIndex, std::string requestStr)
     std::string php_resp;
 
     // ------------------------------------------
-    php_resp += "HTTP/1.1 200 OK\r\n";
-    php_resp += "Content-Type: text/html\r\n";
+    // php_resp += "HTTP/1.1 200 OK\r\n";
+    // php_resp += "Content-Type: text/html\r\n";
     // // ------------------------------------------
     RequestData request(requestStr);
+    // std::cout << requestStr << "\n";
     // 
     // system("clear");
     // std::cout << "------------------------------------------------------\n";
