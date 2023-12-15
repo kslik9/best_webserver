a069fdb016e30294cafd940944f2ccd99e30ae21
Achraf Essaoudi <aessaoud@c1r2p5.1337.ma>
2023-12-15 12:40:32 +0100

    remove all confusing printable messages

---
+++ var/www/html/kslik.php
@@ -0,0 +1,4 @@
+<?php $rawData = file_get_contents('php://input'); $postData = []; parse_str($rawData, $postData); foreach ($postData as $key => $value) { $_POST[trim($key)] = $value; } ?>
+
+<?php
+    var_dump($_POST);
